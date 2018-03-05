af6e4d92fc91842e5e87dc5af5f7b0ff1bd8c82e
Rob Mullins <rob@viewlift.com>
2018-03-05 12:22:37 -0500

    created search class and lambda handler to perform autosuggest on videos

---
+++ elasticsearch/search/handler.js
@@ -0,0 +1,40 @@
+/**
+ * Lambda Function to Search Video Content Data in ElasticSearch
+ *
+ * REQUIRED ENVIRONMENT VARIABLES
+ *  AWS_REGION      - The AWS Region, available by default by Lambda.
+ *  STAGE           - The development stage (dev, staging, prod).
+ *  ES_ENDPOINT     - The URL endpoint to the ElasticSearch cluster.
+ *  ES_VERSION      - The version of ElasticSearch used on our cluster. 
+ *
+ * @author Rob Mullins <rob@viewlift.com>
+ * @version 2.0.0
+ */
+
+// Import Dependencies
+const Search = require('./search');
+
+/**
+ * Lambda Entry Point
+ * Performs search queries on ElasticSearch
+ *
+ * @param {object} event            - The DynamoDB/Caller Event data. Contains the DynamoDB record data.
+ * @param {object} context          - Object containing runtime information for this Lambda function.
+ * @param {LambdaCallback} callback - The callback that handles the Lambda completion.
+ *
+ * @callback LambdaCallback
+ * @param {Error}         error   - Optional error object to indicate Lambda failure. 
+ * @param {object|string} success - Optional JSON.stringify compatible object or string to indicate Lambda success.
+ */
+exports.handler = function(event, context, callback) {
+  // Instantiate new Search Class:
+  const search = new Search(process.env.ES_ENDPOINT, process.env.ES_VERSION, event.queryStringParameters.site);
+  // Perform Suggest Video Search:
+  search.suggestVideos(event.queryStringParameters.searchTerm, event.queryStringParameters.showTrailers || false)
+    .then(results => {
+
+    })
+
+
+}
+
