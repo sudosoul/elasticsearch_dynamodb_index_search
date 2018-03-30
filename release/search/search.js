e6b88deaafb624ba321f8a5861c24616f6842c50
Rob Mullins <rob@viewlift.com>
2018-03-30 12:29:15 -0400

    Search queries for content have been decoupled into individual search queries for each content type (videos, series, audio, photos, events, articles)

--- release/search/search.js
+++ release/search/search.js
@@ -43,58 +43,211 @@ class Search {
       host:         this.endpoint, 
       log:          'error',
       apiVersion:   this.version,
-      keepAlive:    false  // DO NOT CHANGE - LIBRARY CRASHSES WITHOUT THIS SET TO FALSE @see https://github.com/elastic/elasticsearch-js/issues/521 
+      keepAlive:    false  // DO NOT CHANGE - LIBRARY CRASHES WITHOUT THIS SET TO FALSE @see https://github.com/elastic/elasticsearch-js/issues/521 
     }); 
   }
 
   /**
-   * Suggests videos based on the search term / prefix
-   * This is used as the autosuggest videos on sites.
+   * Gets suggestions for all content types by performing individual search queries
+   * on each content type (videos, series, audio, events, articles, photos).
    *
-   * Example: If a user searches inputs 'b' on the search input, this function would be
-   * called and will search for all videos whose specified fields begin with 'b'.
-   * The search fields are specified in the query below in `multi_match` - `fields`.
-   * The `fields` property contains an array of all the fields we wish to search against. 
-   * Appending ^{number} indicates a higher priority/score boost for that field. 
+   * @param    {string} searchTerm      - The search term / search prefix to search by
+   * @return   {Promise.<array,Error>}  - Promise          
+   * @fulfills {array}                  - Array containing all the search results
+   * @rejects  {Error}                  - An ElasticSearch Error
+   */
+  getSuggestions(searchTerm) {
+    return new Promise((fulfill, reject) => {
+      //** Make individual search query for each content type **//
+      const suggestions = [];     // Hold each query results
+      const response    = [];     // Hold the combined/formatted search response
+      suggestions.push(_getVideoSuggestions(searchTerm));     // Query Videos
+      suggestions.push(_getSeriesSuggestions(searchTerm));    // Query Series
+      suggestions.push(_getArticleSuggestions(searchTerm));   // Query Articles
+      suggestions.push(_getEventSuggestions(searchTerm));     // Query Events
+      suggestions.push(_getAudioSuggestions(searchTerm));     // Query Audio
+      suggestions.push(_getPhotoSuggestions(searchTerm));     // Query Photos
+      //** Return combined results **//
+      Promise.all(suggestions)
+        .then(results => {
+          results.forEach(result => {
+            //** If query contains results, push it to final response array **//
+            if (result.hits.hits.length > 0) {
+              result.hits.hits.forEach(hit => {
+                response.push(hit._source.data);
+              });
+            }
+          });
+          fulfill(response); // Return combined search results!
+      }).catch(e => {
+        console.log('Error performing search query - ', e);
+        reject(e);
+      });
+    });
+  }
+
+  /**
+   * Searches for videos that match the searchTerm
    *
-   * For example: ['title^3', 'category^2', 'someOtherField'] would indicate that matches found under the
-   * `title` field are 3 (THREE) times more relevant than matches found in the other fields. 
-   * Matches found for `category` are 2 (TWO) times more relevant than matches found in other fields (except title). 
-   * Therefore the search results would show `title` matches with the highest scores, `category` matches with the 2nd
-   * highest score, and `someOtherField` with the lowest matching scores. 
-   *
-   * NOTE: The search query itself is legacy code that cannot at this time be modified.
-   * For this reason, fields have be indexed with the nGram analyzer, to support the
-   * legacy setup of performing a multi_match from the search prefix, to the specified fields.
-   *
-   * The more correct way to implement this would be to query the _suggest URI of ES. 
-   *
-   * @param {string} searchTerm    - The search term / search-prefix to search for videos by.
-   * @param {boolean} showTrailers - Whether or not to include trailers in the search results. 
-   * @return {Promise.<object,Error>}
-   * @fulfills {array} On successful search, returns an array of items that matched the searchTerm.
-   * @rejects {Error}  Rejects if error performing the search query (NOT IF NO RESULTS FOUND).
-   */ 
-  suggestVideos(searchTerm, showTrailers) {
+   * @param    {string} searchTerm      - The search term / search-prefix to search for videos by.
+   * @return   {Promise.<array,Error>}  - Promise
+   * @fulfills {array}                  - Search Results
+   * @rejects  {Error}                  - An ElasticSearch Error
+   */
+  _getVideoSuggestions(searchTerm) {
     const self = this;
     return this.es.search({
       index: self.index,
       size: 18,
       type: 'content',
       body: {
-        min_score: 15,
+        min_score: 12,
         query: {
           multi_match: {
-            query: searchTerm,
-            type: 'best_fields',
-            analyzer: 'nGram_analyzer',
-            fields: [
-              'videoTitle', 'videoPrimaryCategory', 'videoCategories.name', 'videoPeople.name', 'videoTags.name',
-              'seriesTitle', 'seriesPrimaryCategory', 'seriesCategories.name', 'seriesPeople.name', 'seriesTags.name',
-              'articleTitle', 'articleAuthor', 'articlePrimaryCategory', 'articleCategories.name',  'articleTags.name',
-              'eventTitle', 'eventPrimaryCategory', 'eventCategories.name',  'eventTags.name',
+            query    : searchTerm,
+            type     : 'best_fields',
+            analyzer : 'nGram_analyzer',
+            fields   : ['videoTitle', 'videoPrimaryCategory', 'videoCategories.name', 'videoPeople.name', 'videoTags.name']
+          }
+        }
+      }
+    });
+  }
 
-            ]
+  /**
+   * Searches for series that match the searchTerm
+   *
+   * @param    {string} searchTerm      - The search term / search-prefix to search for series by.
+   * @return   {Promise.<array,Error>}  - Promise
+   * @fulfills {array}                  - Search Results
+   * @rejects  {Error}                  - An ElasticSearch Error
+   */
+  _getSeriesSuggestions(searchTerm) {
+    const self = this;
+    return this.es.search({
+      index: self.index,
+      size: 18,
+      type: 'content',
+      body: {
+        min_score: 12,
+        query: {
+          multi_match: {
+            query    : searchTerm,
+            type     : 'best_fields',
+            analyzer : 'nGram_analyzer',
+            fields   : ['seriesTitle', 'seriesPrimaryCategory', 'seriesCategories.name', 'seriesPeople.name', 'seriesTags.name']
+          }
+        }
+      }
+    });
+  }
+
+  /**
+   * Searches for articles that match the searchTerm
+   *
+   * @param    {string} searchTerm      - The search term / search-prefix to search for articles by.
+   * @return   {Promise.<array,Error>}  - Promise
+   * @fulfills {array}                  - Search Results
+   * @rejects  {Error}                  - An ElasticSearch Error
+   */
+  _getArticleSuggestions(searchTerm) {
+    const self = this;
+    return this.es.search({
+      index: self.index,
+      size: 18,
+      type: 'content',
+      body: {
+        min_score: 12,
+        query: {
+          multi_match: {
+            query    : searchTerm,
+            type     : 'best_fields',
+            analyzer : 'nGram_analyzer',
+            fields   : ['articleTitle', 'articleAuthor', 'articlePrimaryCategory', 'articleCategories.name', 'articleTags.name']
+          }
+        }
+      }
+    });
+  }
+
+  /**
+   * Searches for events that match the searchTerm
+   *
+   * @param    {string} searchTerm      - The search term / search-prefix to search for events by.
+   * @return   {Promise.<array,Error>}  - Promise
+   * @fulfills {array}                  - Search Results
+   * @rejects  {Error}                  - An ElasticSearch Error
+   */
+  _getEventSuggestions(searchTerm) {
+    const self = this;
+    return this.es.search({
+      index: self.index,
+      size: 18,
+      type: 'content',
+      body: {
+        min_score: 12,
+        query: {
+          multi_match: {
+            query    : searchTerm,
+            type     : 'best_fields',
+            analyzer : 'nGram_analyzer',
+            fields   : ['eventTitle', 'eventPrimaryCategory', 'eventCategories.name', 'eventTags.name']
+          }
+        }
+      }
+    });
+  }
+
+  /**
+   * Searches for audio that match the searchTerm
+   *
+   * @param    {string} searchTerm      - The search term / search-prefix to search for audio by.
+   * @return   {Promise.<array,Error>}  - Promise
+   * @fulfills {array}                  - Search Results
+   * @rejects  {Error}                  - An ElasticSearch Error
+   */
+  _getAudioSuggestions(searchTerm) {
+    const self = this;
+    return this.es.search({
+      index: self.index,
+      size: 18,
+      type: 'content',
+      body: {
+        min_score: 12,
+        query: {
+          multi_match: {
+            query    : searchTerm,
+            type     : 'best_fields',
+            analyzer : 'nGram_analyzer',
+            fields   : ['audioTitle', 'audioAuthor', 'audioPrimaryCategory', 'audioCategories.name', 'audioTags.name']
+          }
+        }
+      }
+    });
+  }
+
+  /**
+   * Searches for photos that match the searchTerm
+   *
+   * @param    {string} searchTerm      - The search term / search-prefix to search for photos by.
+   * @return   {Promise.<array,Error>}  - Promise
+   * @fulfills {array}                  - Search Results
+   * @rejects  {Error}                  - An ElasticSearch Error
+   */
+  _getPhotoSuggestions(searchTerm) {
+    const self = this;
+    return this.es.search({
+      index: self.index,
+      size: 18,
+      type: 'content',
+      body: {
+        min_score: 12,
+        query: {
+          multi_match: {
+            query    : searchTerm,
+            type     : 'best_fields',
+            analyzer : 'nGram_analyzer',
+            fields   : ['photoTitle', 'photoAuthor', 'photoPrimaryCategory', 'photoCategories.name', 'photoTags.name']
           }
         }
       }
@@ -103,6 +256,6 @@ class Search {
 
 }
 
-// Expose this Class
+// Expose this Search Class
 module.exports = Search;
 
